91d338963134b7be338d9b92ea118c387959a0c6
atharvakadam <atharvakadam@gmail.com>
2020-06-29 18:44:08 -0700

    Finished part 7->e

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      anecdotes: [],
+      current: 0
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:3001/anecdotes').then(response => {
+      this.setState({ anecdotes: response.data })
+    })
+  }
+  
+  handleClick = () => {
+    const current = Math.floor(
+      Math.random() *  (this.state.anecdotes.length - 1)
+    )
+    this.setState({current})
+  }
+
+  render(){
+
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecodotes ...</div>
+    }
+
+    return(
+      <div>
+        <h1>anecdote of the day</h1>
+        <div>
+        {this.state.anecdotes[this.state.current].content}
+        </div>
+        <button onClick={this.handleClick}>next</button>
+      </div>
+    )
+  }
 }
 
 export default App;
